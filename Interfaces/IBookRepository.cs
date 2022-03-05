ea2a9bd6861703d40a86af6f62243c0d14629551
Mimas <michal.maselko@awesome-ind.com>
2022-03-05 20:44:50 +0100

    Added interfaes and communication with db

---
+++ Interfaces/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using LibApp.Models;
+using System.Collections.Generic;
+
+namespace LibApp.Interfaces
+{
+    public interface IBookRepository
+    {
+        IEnumerable<Book> GetBooks();
+        Book GetBookById(int bookId);
+        void AddBook(Book book);
+        void UpdateBook(Book book);
+        void DeleteBook(int bookId);
+
+    }
+}
---
+++ Repositories/BookRepository.cs
@@ -0,0 +1,45 @@
+﻿using LibApp.Data;
+using LibApp.Interfaces;
+using LibApp.Models;
+using System.Collections.Generic;
+
+namespace LibApp.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBook(Book book)
+        {
+           _context.Books.Add(book);
+        }
+
+        public void DeleteBook(int bookId)
+        {
+           _context.Books.Remove(GetBookById(bookId));
+        }
+
+        public Book GetBookById(int bookId)
+        {
+            return _context.Books.Find(bookId);
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return _context.Books;
+        }
+
+        public void UpdateBook(Book book)
+        {
+            throw new System.NotImplementedException();
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
